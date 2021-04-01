c2e3b78f0a6ec89944e2b23f91e68201c55bded3
RyanFarnan8 <76129011+RyanFarnan8@users.noreply.github.com>
2021-04-01 13:04:46 +0100

    Version 1 ------- Added County Entity , with Client Controller and Tradesman Controller

---
+++ src/Controller/ClientController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+
+/**
+ * @Route("/client", name="client")
+ * @IsGranted("ROLE_CLIENT")
+ */
+
+class ClientController extends AbstractController
+{
+    /**
+     * @Route("/client", name="client")
+     */
+    public function index(): Response
+    {
+        return $this->render('client/index.html.twig', [
+            'controller_name' => 'ClientController',
+        ]);
+    }
+}
